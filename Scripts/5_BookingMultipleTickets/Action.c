f0fe8ce734809bc91e473609e90fb052541af9b0
Ren <75898690+yuutsuren@users.noreply.github.com>
2021-06-01 19:06:06 +0300

    Create Action.c

---
+++ Scripts/5_BookingMultipleTickets/Action.c
@@ -0,0 +1,365 @@
+Action()
+{
+	lr_start_transaction("5_BookingMultipleTickets");
+	
+	lr_start_transaction("visitSite");
+	
+	web_reg_find("Text=Welcome to the Web Tours site.", LAST);
+
+	web_set_sockets_option("SSL_VERSION", "AUTO");
+
+/*Correlation comment - Do not change!  Original value='131413.8036452zHfQfQHpfcftAffHpfifVf' Name ='userSession' Type ='ResponseBased'*/
+	web_reg_save_param_attrib(
+		"ParamName=userSession",
+		"TagName=input",
+		"Extract=value",
+		"Name=userSession",
+		"Type=hidden",
+		SEARCH_FILTERS,
+		"IgnoreRedirections=No",
+		"RequestUrl=*/nav.pl*",
+		LAST);
+
+	web_url("WebTours", 
+		"URL=http://localhost:1080/WebTours/", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		LAST);
+		
+	lr_end_transaction("visitSite", LR_AUTO);
+
+	lr_think_time(19);
+
+	lr_start_transaction("loginPassword");
+	
+	web_reg_find("Text=Welcome, <b>{login}</b>, to the Web Tours reservation pages.", LAST);
+	
+/*Correlation comment - Do not change!  Original value='flights' Name ='in' Type ='ResponseBased'*/
+	web_reg_save_param_regexp(
+		"ParamName=in",
+		"RegExp=images/(.*?)\\.gif",
+		SEARCH_FILTERS,
+		"Scope=Body",
+		"IgnoreRedirections=No",
+		"RequestUrl=*/nav.pl*",
+		LAST);
+
+/*Correlation comment - Do not change!  Original value='welcome' Name ='page' Type ='ResponseBased'*/
+	web_reg_save_param_regexp(
+		"ParamName=page",
+		"RegExp=A\\ HREF=\"(.*?)\\.pl",
+		SEARCH_FILTERS,
+		"Scope=Body",
+		"IgnoreRedirections=No",
+		"RequestUrl=*/nav.pl*",
+		LAST);
+
+	web_submit_data("login.pl",
+		"Action=http://localhost:1080/cgi-bin/login.pl",
+		"Method=POST",
+		"TargetFrame=body",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?in=home",
+		"Snapshot=t2.inf",
+		"Mode=HTML",
+		ITEMDATA,
+		"Name=userSession", "Value={userSession}", ENDITEM,
+		"Name=username", "Value={login}", ENDITEM,
+		"Name=password", "Value={password}", ENDITEM,
+		"Name=JSFormSubmit", "Value=off", ENDITEM,
+		"Name=login.x", "Value=43", ENDITEM,
+		"Name=login.y", "Value=11", ENDITEM,
+		LAST);
+
+	lr_end_transaction("loginPassword",LR_AUTO);
+
+	lr_think_time(5);
+
+	lr_start_transaction("clickFlightsButton");
+	
+	web_reg_find("Text=Web Tours", LAST);
+
+/*Correlation comment - Do not change!  Original value='roundtrip' Name ='.cgifields' Type ='ResponseBased'*/
+	web_reg_save_param_attrib(
+		"ParamName=.cgifields",
+		"TagName=input",
+		"Extract=name",
+		"Type=checkbox",
+		SEARCH_FILTERS,
+		"IgnoreRedirections=No",
+		"RequestUrl=*/reservations.pl*",
+		LAST);
+
+	web_url("Search Flights Button",
+		"URL=http://localhost:1080/cgi-bin/{page}.pl?page=search",
+		"TargetFrame=body",
+		"Resource=0",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home",
+		"Snapshot=t3.inf",
+		"Mode=HTML",
+		LAST);
+
+	lr_end_transaction("clickFlightsButton",LR_AUTO);
+
+	lr_think_time(21);
+
+	lr_start_transaction("flightSettings");
+	
+	web_reg_find("Text=Find Flight", LAST);
+
+/*Correlation comment - Do not change!  Original value='501;245;05/30/2021' Name ='outboundFlight' Type ='ResponseBased'*/
+	web_reg_save_param_attrib(
+		"ParamName=outboundFlight",
+		"TagName=input",
+		"Extract=value",
+		"Name=outboundFlight",
+		"Type=radio",
+		SEARCH_FILTERS,
+		"IgnoreRedirections=No",
+		LAST);
+
+	web_submit_data("reservations.pl",
+		"Action=http://localhost:1080/cgi-bin/reservations.pl",
+		"Method=POST",
+		"TargetFrame=",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/reservations.pl?page={page}",
+		"Snapshot=t4.inf",
+		"Mode=HTML",
+		ITEMDATA,
+		"Name=advanceDiscount", "Value=0", ENDITEM,
+		"Name=depart", "Value={departCity}", ENDITEM,
+		"Name=departDate", "Value={departDate}", ENDITEM,
+		"Name=arrive", "Value={arrivalCity}", ENDITEM,
+		"Name=returnDate", "Value={returnDate}", ENDITEM,
+		"Name=numPassengers", "Value=1", ENDITEM,
+		"Name=seatPref", "Value={seatPref}", ENDITEM,
+		"Name=seatType", "Value={seatType}", ENDITEM,
+		"Name=.cgifields", "Value={.cgifields}", ENDITEM,
+		"Name=.cgifields", "Value={seatType}", ENDITEM,
+		"Name=.cgifields", "Value={seatPref}", ENDITEM,
+		"Name=findFlights.x", "Value=45", ENDITEM,
+		"Name=findFlights.y", "Value=11", ENDITEM,
+		LAST);
+
+	lr_end_transaction("flightSettings",LR_AUTO);
+
+	lr_think_time(19);
+
+	lr_start_transaction("flightSelection");
+	
+	web_reg_find("Text=Payment Details", LAST);
+
+	web_submit_data("reservations.pl_2",
+		"Action=http://localhost:1080/cgi-bin/reservations.pl",
+		"Method=POST",
+		"TargetFrame=",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/reservations.pl",
+		"Snapshot=t5.inf",
+		"Mode=HTML",
+		ITEMDATA,
+		"Name=outboundFlight", "Value={outboundFlight}", ENDITEM,
+		"Name=numPassengers", "Value=", ENDITEM,
+		"Name=advanceDiscount", "Value=0", ENDITEM,
+		"Name=seatType", "Value={seatType}", ENDITEM,
+		"Name=seatPref", "Value={seatPref}", ENDITEM,
+		"Name=reserveFlights.x", "Value=43", ENDITEM,
+		"Name=reserveFlights.y", "Value=8", ENDITEM,
+		LAST);
+
+	lr_end_transaction("flightSelection",LR_AUTO);
+
+	lr_think_time(17);
+
+	lr_start_transaction("paymentData");
+	
+	web_reg_find("Text=Thank you for booking through Web Tours.", LAST);
+
+	web_submit_data("reservations.pl_3",
+		"Action=http://localhost:1080/cgi-bin/reservations.pl",
+		"Method=POST",
+		"TargetFrame=",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/reservations.pl",
+		"Snapshot=t6.inf",
+		"Mode=HTML",
+		ITEMDATA,
+		"Name=firstName", "Value={firstName}", ENDITEM,
+		"Name=lastName", "Value={lastname}", ENDITEM,
+		"Name=address1", "Value={adress}", ENDITEM,
+		"Name=address2", "Value={cityStateZip}", ENDITEM,
+		"Name=pass1", "Value={firstName} {lastName}", ENDITEM,
+		"Name=creditCard", "Value={creditCard}", ENDITEM,
+		"Name=expDate", "Value={expDate}", ENDITEM,
+		"Name=saveCC", "Value=on", ENDITEM,
+		"Name=oldCCOption", "Value=on", ENDITEM,
+		"Name=numPassengers", "Value=", ENDITEM,
+		"Name=seatType", "Value={seatType}", ENDITEM,
+		"Name=seatPref", "Value={seatPref}", ENDITEM,
+		"Name=outboundFlight", "Value={outboundFlight}", ENDITEM,
+		"Name=advanceDiscount", "Value=0", ENDITEM,
+		"Name=returnFlight", "Value=", ENDITEM,
+		"Name=JSFormSubmit", "Value=off", ENDITEM,
+		"Name=.cgifields", "Value=saveCC", ENDITEM,
+		"Name=buyFlights.x", "Value=49", ENDITEM,
+		"Name=buyFlights.y", "Value=10", ENDITEM,
+		LAST);
+
+	lr_end_transaction("paymentData",LR_AUTO);
+
+	lr_think_time(13);
+
+	lr_start_transaction("clickBookAnother");
+	
+	web_reg_find("Text=Find Flight", LAST);
+
+	web_submit_data("reservations.pl_4", 
+		"Action=http://localhost:1080/cgi-bin/reservations.pl", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/reservations.pl", 
+		"Snapshot=t7.inf", 
+		"Mode=HTML", 
+		ITEMDATA, 
+		"Name=Book Another.x", "Value=41", ENDITEM, 
+		"Name=Book Another.y", "Value=10", ENDITEM, 
+		LAST);
+
+	lr_end_transaction("clickBookAnother",LR_AUTO);
+
+	lr_think_time(55);
+
+	lr_start_transaction("flightSettings");
+	
+	web_reg_find("Text=Find Flight", LAST);
+
+/*Correlation comment - Do not change!  Original value='543;953;05/30/2021' Name ='outboundFlight_1' Type ='ResponseBased'*/
+	web_reg_save_param_attrib(
+		"ParamName=outboundFlight",
+		"TagName=input",
+		"Extract=value",
+		"Name=outboundFlight",
+		"Type=radio",
+		SEARCH_FILTERS,
+		"IgnoreRedirections=No",
+		LAST);
+
+	web_submit_data("reservations.pl_5",
+		"Action=http://localhost:1080/cgi-bin/reservations.pl",
+		"Method=POST",
+		"TargetFrame=",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/reservations.pl",
+		"Snapshot=t8.inf",
+		"Mode=HTML",
+		ITEMDATA,
+		"Name=advanceDiscount", "Value=0", ENDITEM,
+		"Name=depart", "Value={departCity}", ENDITEM,
+		"Name=departDate", "Value={departDate}", ENDITEM,
+		"Name=arrive", "Value={arrivalCity}", ENDITEM,
+		"Name=returnDate", "Value={returnDate}", ENDITEM,
+		"Name=numPassengers", "Value=1", ENDITEM,
+		"Name=seatPref", "Value={seatPref}", ENDITEM,
+		"Name=seatType", "Value={seatType}", ENDITEM,
+		"Name=.cgifields", "Value={.cgifields}", ENDITEM,
+		"Name=.cgifields", "Value={seatType}", ENDITEM,
+		"Name=.cgifields", "Value={seatPref}", ENDITEM,
+		"Name=findFlights.x", "Value=46", ENDITEM,
+		"Name=findFlights.y", "Value=11", ENDITEM,
+		LAST);
+
+	lr_end_transaction("flightSettings",LR_AUTO);
+
+	lr_think_time(16);
+
+	lr_start_transaction("flightSelection");
+	
+	web_reg_find("Text=Payment Details", LAST);
+
+	web_submit_data("reservations.pl_6",
+		"Action=http://localhost:1080/cgi-bin/reservations.pl",
+		"Method=POST",
+		"TargetFrame=",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/reservations.pl",
+		"Snapshot=t9.inf",
+		"Mode=HTML",
+		ITEMDATA,
+		"Name=outboundFlight", "Value={outboundFlight}", ENDITEM,
+		"Name=numPassengers", "Value=", ENDITEM,
+		"Name=advanceDiscount", "Value=0", ENDITEM,
+		"Name=seatType", "Value={seatType}", ENDITEM,
+		"Name=seatPref", "Value={seatPref}", ENDITEM,
+		"Name=reserveFlights.x", "Value=49", ENDITEM,
+		"Name=reserveFlights.y", "Value=11", ENDITEM,
+		LAST);
+
+	lr_end_transaction("flightSelection",LR_AUTO);
+
+	lr_think_time(13);
+
+	lr_start_transaction("paymentData");
+	
+	web_reg_find("Text=Thank you for booking through Web Tours.", LAST);
+
+	web_submit_data("reservations.pl_7",
+		"Action=http://localhost:1080/cgi-bin/reservations.pl",
+		"Method=POST",
+		"TargetFrame=",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/reservations.pl",
+		"Snapshot=t10.inf",
+		"Mode=HTML",
+		ITEMDATA,
+		"Name=firstName", "Value={firstName}", ENDITEM,
+		"Name=lastName", "Value={lastName}", ENDITEM,
+		"Name=address1", "Value={adress}", ENDITEM,
+		"Name=address2", "Value={cityStateZip}", ENDITEM,
+		"Name=pass1", "Value={firstName} {lastName}", ENDITEM,
+		"Name=creditCard", "Value={creditCard}", ENDITEM,
+		"Name=expDate", "Value={expDate}", ENDITEM,
+		"Name=saveCC", "Value=on", ENDITEM,
+		"Name=oldCCOption", "Value=on", ENDITEM,
+		"Name=numPassengers", "Value=", ENDITEM,
+		"Name=seatType", "Value={seatType}", ENDITEM,
+		"Name=seatPref", "Value={seatPref}", ENDITEM,
+		"Name=outboundFlight", "Value={outboundFlight}", ENDITEM,
+		"Name=advanceDiscount", "Value=0", ENDITEM,
+		"Name=returnFlight", "Value=", ENDITEM,
+		"Name=JSFormSubmit", "Value=off", ENDITEM,
+		"Name=.cgifields", "Value=saveCC", ENDITEM,
+		"Name=buyFlights.x", "Value=50", ENDITEM,
+		"Name=buyFlights.y", "Value=12", ENDITEM,
+		LAST);
+
+	lr_end_transaction("paymentData",LR_AUTO);
+	
+	lr_think_time(9);
+
+	lr_start_transaction("logOut");
+	
+	web_reg_find("Text=Web Tours", LAST);
+
+	web_url("SignOff Button",
+		"URL=http://localhost:1080/cgi-bin/{page}.pl?signOff=1",
+		"TargetFrame=body",
+		"Resource=0",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in={in}",
+		"Snapshot=t11.inf",
+		"Mode=HTML",
+		LAST);
+
+	lr_end_transaction("logOut",LR_AUTO);
+	
+	lr_end_transaction("5_BookingMultipleTickets", LR_AUTO);
+
+	return 0;
+}
